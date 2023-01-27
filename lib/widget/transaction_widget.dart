dc0b6f7ebc6c53286f82eaec92057c2d1c4751e1
zionnite <nosakharee.16@gmail.com>
2023-01-27 02:09:26 +0100

    first initial commit of oga Bliss

--- lib/widget/transaction_widget.dart
+++ lib/widget/transaction_widget.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_slidable/flutter_slidable.dart';
 
 import '../util/currency_formatter.dart';
 
@@ -18,50 +17,26 @@ class transactionWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SlidableAutoCloseBehavior(
-      closeWhenOpened: true,
-      child: Slidable(
-        startActionPane: const ActionPane(
-          motion: BehindMotion(),
+    return Card(
+      margin: const EdgeInsets.only(
+        left: 10,
+        right: 10,
+        bottom: 5,
+      ),
+      child: Container(
+        child: Column(
           children: [
-            SlidableAction(
-              onPressed: null,
-              backgroundColor: Color(0xFFFE4A49),
-              foregroundColor: Colors.white,
-              icon: Icons.delete,
-              label: 'Delete',
-            ),
-            SlidableAction(
-              onPressed: null,
-              backgroundColor: Color(0xFF21B7CA),
-              foregroundColor: Colors.white,
-              icon: Icons.share,
-              label: 'Share',
-            ),
+            ListTile(
+              leading: leadingIcon,
+              title: Text(
+                CurrencyFormatter.getCurrencyFormatter(
+                  amount: "${amount}",
+                ),
+              ),
+              subtitle: Text(date),
+              trailing: trailingIcon,
+            )
           ],
-        ),
-        child: Card(
-          margin: const EdgeInsets.only(
-            left: 10,
-            right: 10,
-            bottom: 5,
-          ),
-          child: Container(
-            child: Column(
-              children: [
-                ListTile(
-                  leading: leadingIcon,
-                  title: Text(
-                    CurrencyFormatter.getCurrencyFormatter(
-                      amount: "${amount}",
-                    ),
-                  ),
-                  subtitle: Text(date),
-                  trailing: trailingIcon,
-                )
-              ],
-            ),
-          ),
         ),
       ),
     );
